54200b14380f2ba7275e1a9c8cd38cec084a5cab
may <wxm112@me.com>
2017-11-07 20:02:44 +1100

    send ga event after geting the products

--- src/GaWrapper.js
+++ src/GaWrapper.js
@@ -3,15 +3,17 @@ import React from 'react';
 function withGaWrapper(WrappedComponent) {
     return class extends React.Component {
         componentWillMount() {
-            const script = document.createElement("script");            
+            const script = document.createElement("script");
             script.innerHTML = `\nwindow.dataLayer = [];\n`;
             document.body.appendChild(script);
         }
 
         sendGaEcommerce(products) {
-            products.forEach(function(product) {
-                window.dataLayer.push(product);
-            }, this);
+            if (products.length > 0) {
+                products.forEach((product) => {
+                    window.dataLayer.push(product);
+                }, this);
+            }
         }
 
         render() {
